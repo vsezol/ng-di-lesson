628823c88de808445a434dd25cf177e7aa772e3f
Andrey Nikonenko <sonanted@gmail.com>
2024-08-21 19:25:50 +0300

    fix: services & app component

--- src/app/id-generator.service.ts
+++ src/app/id-generator.service.ts
@@ -1,5 +1,12 @@
+import { Injectable } from '@angular/core';
 import { IdGenerator } from './id-generator';
 
 // Доработайте этот код
+@Injectable()
+export class IdGeneratorService extends IdGenerator {
+  private id: number = 0;
 
-export class IdGeneratorService extends IdGenerator {}
+  generate(): number {
+    return ++this.id;
+  }
+}
