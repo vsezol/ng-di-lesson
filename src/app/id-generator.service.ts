d4ba0124c73265e7801d10b185a940e56b58ffc4
Данил Скороходов <skorohodovdanil07@gmail.com>
2024-08-21 19:41:40 +0300

    feat: full test

--- src/app/id-generator.service.ts
+++ src/app/id-generator.service.ts
@@ -1,5 +1,13 @@
 import { IdGenerator } from './id-generator';
-
+import { Injectable } from '@angular/core';
 // Доработайте этот код
 
-export class IdGeneratorService extends IdGenerator {}
+@Injectable({
+    providedIn: "root"
+  })
+export class IdGeneratorService extends IdGenerator {
+    #id = 1;
+    override generate(): number {
+        return this.#id++;
+      }
+}
